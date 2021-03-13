687966c66472362b7bcd7078d7775c6311165fc3
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-03-13 16:34:17 +0600

    JQuery added

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
           $result = $temp;
           $per = '%';
 
-          $sql="INSERT INTO  lovers(name1, name2, percentage, ip) VALUES(:name1,:name2,:percentage, :ip)";
+          $sql="INSERT INTO  lovers_list(name1, name2, percentage, ip) VALUES(:name1,:name2,:percentage, :ip)";
           $query = $dbh->prepare($sql);
           $query->bindParam(':name1',$name1,PDO::PARAM_STR);
           $query->bindParam(':name2',$name2,PDO::PARAM_STR);
@@ -129,7 +129,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
     </head>
     <body>
         <div class="container_head">
@@ -170,8 +170,7 @@
 
         
         
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jquery-3.6.0.js"></script>
         </body>
 </html>
